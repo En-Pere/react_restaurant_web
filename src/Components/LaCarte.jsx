2b39ab58fd147b9274a563ad679bf662cacc9157
EnPere <pereruba@gmail.com>
2023-01-20 21:01:27 +0100

    menus done

--- src/Components/LaCarte.jsx
+++ src/Components/LaCarte.jsx
@@ -6,19 +6,18 @@ function LaCarte() {
     <div className="lacarte-background">
       : La Carte :
       <div className="main-container-lacarte">
-        <div className="menu1">
-          <h1>Menu1</h1>
-          <h2>
-            Thon germon en tartare , concombre et coriandre ou Sardine en
-            beignet , tomate et menthe Lotte tiède aux herbes d’été ou palourdes
-            à l’ail Gâteau au chocolat ou salade d’agrumes
-          </h2>
+        <div className="menu1">menu 1</div>
+        <div className="menu2y3">
+          <div className="menu2">menu 2</div>
+          <div className="menu3">menu 3</div>
         </div>
-        <div className="menu2">menu 2</div>
-        <div className="menu3">menu 3</div>
-        <div className="menu4">menu 4</div>
-        <div className="menu5">menu 5</div>
-        <div className="menu6">menu 6</div>
+        <div className="menu4y5y6">
+          <div className="menu4">menu 4</div>
+          <div className="menu5y6">
+            <div className="menu5">menu 5</div>
+            <div className="menu6">menu 6</div>
+          </div>
+        </div>
       </div>
     </div>
   );
